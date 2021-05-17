335c9dfb45c3e38153138037f033db8c2283f0d1
Sergeyxow <Sergeyxow@yandex.ru>
2021-05-17 14:31:17 +0300

    LevelBox class

---
+++ Assets/Scripts/UI/LevelBox.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using DefaultNamespace.Common;
+using TMPro;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+
+namespace DefaultNamespace.UI
+{
+    public class LevelBox : MonoBehaviour, IPointerDownHandler
+    {
+        [SerializeField] private TextMeshProUGUI _titleTextBox;
+        [SerializeField] private StarsBox _starsBox;
+        [SerializeField] private Image _backgroundImage;
+        [SerializeField] private Image _avatarImage;
+
+        public event Action<int> Clicked;
+        
+        private int _id;
+        private LevelDescription _levelDescription;
+        private LevelStatistics _levelStatistics;
+
+        public void Init(int id, LevelDescription levelDescription, LevelStatistics levelStatistics)
+        {
+            _id = id;
+            _levelStatistics = levelStatistics;
+            _levelDescription = levelDescription;
+            
+            UpdateView();
+        }
+
+        public void UpdateView()
+        {
+            _titleTextBox.text = _levelDescription.Name;
+            _backgroundImage.sprite = _levelDescription.BackgroundSprite;
+            _avatarImage.sprite = _levelDescription.AvatarSprite;
+            _starsBox.Show(_levelStatistics.stars);
+        }
+
+        public void OnPointerDown(PointerEventData eventData)
+        {
+            Clicked?.Invoke(_id);
+        }
+    }
+}
