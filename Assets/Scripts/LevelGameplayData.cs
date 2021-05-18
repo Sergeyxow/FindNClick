9363fd20a53996b37a2262d01f3589bba52a7a20
Sergeyxow <Sergeyxow@yandex.ru>
2021-05-18 12:07:27 +0300

    LevelGameplayDataCollection

---
+++ Assets/Scripts/LevelGameplayData.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+
+namespace DefaultNamespace
+{
+    public class LevelGameplayData
+    {
+        public Sprite Background;
+        public int ClicksToKill;
+    }
+}
---
+++ Assets/Scripts/LevelGameplayDataCollection.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+
+namespace DefaultNamespace
+{
+    public class LevelGameplayDataCollection : ScriptableObject
+    {
+        [SerializeField] private LevelGameplayData[] _levelGameplayDataArray;
+
+        public bool TryGetStatistics(int id, out LevelGameplayData outLevelGameplayData)
+        {
+            if (id < _levelGameplayDataArray.Length)
+            {
+                outLevelGameplayData = _levelGameplayDataArray[id];
+                return true;
+            }
+
+            outLevelGameplayData = null;
+            return false;
+        }
+    }
+}
