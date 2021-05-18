1fa468ea6055133a5f901417bedc4aad2481863c
Sergeyxow <Sergeyxow@yandex.ru>
2021-05-18 12:08:30 +0300

    Enemy Actor

---
+++ Assets/Scripts/EnemyActor.cs
@@ -0,0 +1,17 @@
+﻿using Modules.Health;
+using UnityEngine;
+
+namespace DefaultNamespace
+{
+    public class EnemyActor : MonoBehaviour
+    {
+        [SerializeField] private SpriteRenderer _spriteRenderer;
+        [SerializeField] private HealthComponent _healthComponent;
+        
+        public void Init(Sprite sprite, float maxHealth)
+        {
+            _spriteRenderer.sprite = sprite;
+            _healthComponent.MaxHealth = maxHealth;
+        }
+    }
+}
