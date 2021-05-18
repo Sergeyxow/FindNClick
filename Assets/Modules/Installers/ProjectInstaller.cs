91e389963c2db08437b8e7e0d811d9f53842ff8a
Sergeyxow <Sergeyxow@yandex.ru>
2021-05-18 12:18:24 +0300

    LevelGameplayDataCollection injection

--- Assets/Modules/Installers/ProjectInstaller.cs
+++ Assets/Modules/Installers/ProjectInstaller.cs
@@ -10,15 +10,19 @@ namespace Modules.Installers
     {
         [SerializeField] private GameData _gameData;
         [SerializeField] private LevelCollection _levelCollection;
+        
         [SerializeField] private LevelDescriptionCollection _levelDescriptionCollection;
         [SerializeField] private LevelStatisticsCollection _levelStatisticsCollection;
+        [SerializeField] private LevelGameplayDataCollection _levelGameplayDataCollection;
 
         public override void InstallBindings()
         {
             Container.Bind<GameData>().FromInstance(_gameData).AsSingle();
             Container.Bind<LevelCollection>().FromInstance(_levelCollection).AsSingle();
+            
             Container.Bind<LevelDescriptionCollection>().FromInstance(_levelDescriptionCollection).AsSingle();
             Container.Bind<LevelStatisticsCollection>().FromInstance(_levelStatisticsCollection).AsSingle();
+            Container.Bind<LevelGameplayDataCollection>().FromInstance(_levelGameplayDataCollection).AsSingle();
             
             Container.Bind<SessionData>().To<SessionData>().AsSingle();
             Container.Bind<SaverBase<PlayerData>>().To<JSONSaver<PlayerData>>().AsSingle();
--- Assets/Scripts/Data/LevelGameplayDataCollection.cs
+++ Assets/Scripts/Data/LevelGameplayDataCollection.cs
@@ -2,6 +2,7 @@
 
 namespace DefaultNamespace
 {
+    [CreateAssetMenu(fileName = "LevelGameplayDataCollection", menuName = "LevelGameplayDataCollection")]
     public class LevelGameplayDataCollection : ScriptableObject
     {
         [SerializeField] private LevelGameplayData[] _levelGameplayDataArray;
