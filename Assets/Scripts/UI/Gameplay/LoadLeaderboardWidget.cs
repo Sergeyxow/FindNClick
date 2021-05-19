525e2d9c2410e6896c3fe1903a04de022cbd0a4a
Sergeyxow <Sergeyxow@yandex.ru>
2021-05-19 17:56:52 +0300

    Win screen show leaderboard

---
+++ Assets/Scripts/UI/Gameplay/LoadLeaderboardWidget.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections.Generic;
+using DefaultNamespace.Common;
+using Modules.Data;
+using Modules.UI;
+using Plugins.DataSave;
+using UnityEngine;
+using Zenject;
+
+namespace DefaultNamespace.UI.Gameplay
+{
+    public class LoadLeaderboardWidget : Widget
+    {
+        [SerializeField] private Leaderboard _leaderboard;
+        private SaverBase<LevelStatisticsArray> _levelStatisticsSaver;
+        private SessionData _sessionData;
+
+        [Inject]
+        private void Construct(SaverBase<LevelStatisticsArray> levelStatisticsSaver, SessionData sessionData)
+        {
+            _sessionData = sessionData;
+            _levelStatisticsSaver = levelStatisticsSaver;
+        }
+
+        public override void Show()
+        {
+            base.Show();
+            int levelId = _sessionData.LevelId;
+            List<UserScore> userStatisticsList = _levelStatisticsSaver.Instance.stats[levelId].leaderboard;
+            _leaderboard.UpdateView(userStatisticsList);
+        }
+    }
+}
