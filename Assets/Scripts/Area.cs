6c71ec3d955da696f2ff54a3a9111a2fabef6206
Sergeyxow <Sergeyxow@yandex.ru>
2021-05-20 12:44:48 +0300

    Area class

---
+++ Assets/Scripts/Area.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+
+namespace DefaultNamespace
+{
+    public class Area : MonoBehaviour
+    {
+        [SerializeField] private bool _moveBoundsWithParent = false;
+        [SerializeField] private Vector2 _bounds;
+        private Vector2 _centerAtStart;
+        private bool _started;
+
+        private void Start()
+        {
+            _centerAtStart = transform.position;
+            _started = true;
+        }
+        public Vector2 GetRandomPoint()
+        {
+            Vector2 center = _moveBoundsWithParent ? (Vector2) transform.position : _centerAtStart;
+            Vector2 half = _bounds / 2;
+            var x = Random.Range(center.x - half.x, center.x + half.x);
+            var y = Random.Range(center.y - half.y, center.y + half.y);
+            return new Vector2(x, y);
+        }
+
+        private void OnDrawGizmos()
+        {
+            Gizmos.color = new Color(0.88f, 0f, 1f);
+            Vector3 center;
+            if (_started && !_moveBoundsWithParent)
+            {
+                center = _centerAtStart;
+            }
+            else
+            {
+                center = transform.position;
+            }
+            Gizmos.DrawWireCube(center, _bounds);
+        }
+    }
+}
--- Assets/Scripts/JumpInsideAreaComponent.cs
+++ Assets/Scripts/JumpInsideAreaComponent.cs
@@ -1,55 +1,20 @@
 ﻿using System;
 using UnityEngine;
-using Random = UnityEngine.Random;
 
 namespace DefaultNamespace
 {
     public class JumpInsideAreaComponent : MonoBehaviour
     {
         [SerializeField] public bool CanMove = true;
-        [SerializeField] private bool _moveBoundsWithParent = false;
-        [SerializeField] private Vector2 _bounds;
         [SerializeField] private Transform _target;
-
-        private Vector2 _centerAtStart;
-        private bool _started;
-
-        private void Start()
-        {
-            _centerAtStart = transform.position;
-            _started = true;
-        }
+        [SerializeField] private Area _area;
 
         public void Jump()
         {
             if (!CanMove)
                 return;
             
-            Vector2 center =  _moveBoundsWithParent ? (Vector2) transform.position : _centerAtStart;
-            _target.position = GetRandomPoint(center, _bounds);
-        }
-
-        private Vector2 GetRandomPoint(Vector2 center, Vector2 bounds)
-        {
-            Vector2 half = bounds / 2;
-            var x = Random.Range(center.x - half.x, center.x + half.x);
-            var y = Random.Range(center.y - half.y, center.y + half.y);
-            return new Vector2(x, y);
-        }
-        
-        private void OnDrawGizmos()
-        {
-            Gizmos.color = new Color(0.88f, 0f, 1f);
-            Vector3 center;
-            if (_started && !_moveBoundsWithParent)
-            {
-                center = _centerAtStart;
-            }
-            else
-            {
-                center = transform.position;
-            }
-            Gizmos.DrawWireCube(center, _bounds);
+            _target.position = _area.GetRandomPoint();
         }
     }
 }
