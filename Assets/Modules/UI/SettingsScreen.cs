db17136d1a01a33be6f61b5227cf897ba29c41c0
Sergeyxow <Sergeyxow@yandex.ru>
2021-05-20 18:38:09 +0300

    SettingsScreen

---
+++ Assets/Modules/UI/SettingsScreen.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using DG.Tweening;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace Modules.UI
+{
+    public class SettingsScreen : MonoBehaviour
+    {
+        [SerializeField] private Button _gearButton;
+        [SerializeField] private RectTransform _soundPad;
+        [SerializeField] private RectTransform _soundPadEnabledPoint, _soundPadDisabledPoint;
+
+        private bool _soundPadIsOn;
+        private bool _soundPadIsMoving;
+
+        private void Start()
+        {
+            _gearButton.onClick.AddListener(OnGearClicked);
+        }
+
+        private void OnGearClicked()
+        {
+            if (_soundPadIsMoving)
+                return;
+
+            _soundPadIsOn = !_soundPadIsOn;
+            MoveSoundPad(_soundPadIsOn);
+        }
+
+        private void MoveSoundPad(bool targetState)
+        {
+            _soundPadIsMoving = true;
+            
+            Vector2 anchorPosition = targetState
+                ? _soundPadEnabledPoint.anchoredPosition
+                : _soundPadDisabledPoint.anchoredPosition;
+
+            _soundPad.DOComplete();
+            _soundPad.DOKill();
+            _soundPad.DOAnchorPos(anchorPosition, 0.25f).OnComplete(() => _soundPadIsMoving = false);
+        }
+    }
+}
